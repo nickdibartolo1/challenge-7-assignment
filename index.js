4dd338dee55a1dd8f08e6dfca049eac9b5eb18b9
nickdibartolo1 <dibartolonick0518@gmail.com>
2022-08-20 20:41:12 -0400

    installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
 const questions = [];
+    
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +15,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
