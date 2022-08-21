2f1db954eb7ce9ecd6c2cf3be5bf40bca8ce2abd
nickdibartolo1 <dibartolonick0518@gmail.com>
2022-08-21 16:26:36 -0400

    added function to generate questions, added questions

--- index.js
+++ index.js
@@ -3,7 +3,62 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: "projectTitle",
+            message: "What is the name of the project?",
+        },
+        {
+            type: 'input',
+            name: "description",
+            message: "Give a description of the project?",
+        },
+        {
+            type: 'input',
+            name: "motivation",
+            message: "What is the motivation for this project?",
+        },
+        {
+            type: 'input',
+            name: "installation",
+            message: "Describe if there is any installation processes.",
+        },
+        {
+            type: 'input',
+            name: "usage",
+            message: "What is the use case for this project?",
+        },
+        {
+            type: 'input',
+            name: "license",
+            message: "What license, if any, do you have for this project?",
+        },
+        {
+            type: 'input',
+            name: "contributors",
+            message: "Who are the contributors of this project?",
+        },
+        {
+            type: 'input',
+            name: "tests",
+            message: "Are there inculded tests?",
+        },
+        {
+            type: 'input',
+            name: "githubUsername",
+            message: "What is your github username?",
+        },
+        {
+            type: 'input',
+            name: "Email",
+            message: "What is your email?",
+        }
+    ])
+};
+
+questionUser();
     
 
 
